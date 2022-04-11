8a0e2cdbd4d889e2e88fcbeedffec106454c1916
alex079 <>
2022-04-11 17:26:12 +0200

    fix watchdog hint

--- src/TinyWatchdog.cpp
+++ src/TinyWatchdog.cpp
@@ -20,6 +20,19 @@ void TinyWatchdog::disarm() {
   watchdogDisarm();
 }
 
+/*
+Watchdog prescaler values
+0 -> 16 ms
+1 -> 32 ms
+2 -> 64 ms
+3 -> 0.125 s
+4 -> 0.25 s
+5 -> 0.5 s
+6 -> 1.0 s
+7 -> 2.0 s
+8 -> 4.0 s
+9 -> 8.0 s
+*/
 TinyWatchdog Watchdog;
 
 inline void _ISRWatchdogEventCallback() {
--- src/TinyWatchdog.h
+++ src/TinyWatchdog.h
@@ -5,19 +5,6 @@
 
 typedef void (*WatchdogCallback)();
 
-/*
-Watchdog prescaler values
-0 -> 16 ms
-1 -> 32 ms
-2 -> 64 ms
-3 -> 0.125 s
-4 -> 0.25 s
-5 -> 0.5 s
-6 -> 1.0 s
-7 -> 2.0 s
-8 -> 4.0 s
-9 -> 8.0 s
-*/
 class TinyWatchdog {
   friend void _ISRWatchdogEventCallback();
   public:
